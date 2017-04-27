56187b8f7ebe02730f92004369b483ea4e544da7
Constantine Cherkashyn <constantine.v.cherkashyn@gmail.com>
2017-04-27 19:14:59 +0300

    Fist form

---
+++ Bayes_net/Bayes_net/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Bayes_net
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Save = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(12, 12);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(1066, 700);
+            this.Save.TabIndex = 0;
+            this.Save.TabStop = false;
+            this.Save.Text = "groupBox1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1182, 749);
+            this.Controls.Add(this.Save);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox Save;
+    }
+}
+
---
+++ Bayes_net/Bayes_net/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;                                                                                                                                                                                                           ///////////////////////////////////
+using System.Collections.Generic;                                                                                                                                                                                       ////С17С17С17///С17///С17С17С17////
+using System.ComponentModel;                                                                                                                                                                                            ////С17///С17///С17/////////С17////                        
+using System.Data;                                                                                                                                                                                                      ////С17/////////С17//////С17///////                        
+using System.Drawing;                                                                                                                                                                                                   ////С17///С17///С17/////С17////////                            
+using System.Linq;                                                                                                                                                                                                      ////С17С17С17///С17/////С17////////                            
+using System.Text;                                                                                                                                                                                                      ///////////////////////////////////
+using System.Threading.Tasks;                                                                                                                                                                                           //////Constantine Seventeen////////
+using System.Windows.Forms;                                                                                                                                                                                             ///////////////////////////////////
+
+
+namespace Bayes_net
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        TextBox[,] tb = new TextBox[30, 30];
+
+        /// </summary>
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            int x = 7, y = 20;
+            for (int j = 0; j < 30; j++)
+            {
+                for (int i = 0; i < 30; i++)
+                {
+                    tb[i, j] = new TextBox() //свойства текстбоксов
+                    {
+                        Location = new Point(x, y),
+                        Text = "0.0",
+                        Size = new Size(24, 20)
+                    };
+                    this.Save.Controls.Add(tb[i, j]);
+                    x += 25;
+                }
+                x = 7; y += 20;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
